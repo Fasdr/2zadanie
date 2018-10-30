05ea851b7cbc5f857a531eb1ade7fd2ba5503cd2
saa19 <saaa1997@gmail.com>
2018-10-31 00:49:46 +0700

    heh

--- Main.py
+++ Main.py
@@ -1,3 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from math import exp
+from math import sin, cos
+
+h = 0.1
+t = 0.1
+
+nx = int(1/h)+1
+nt = int(1/t)+1
+
+u = np.zeros((nt, nx))
+c = np.zeros((nt, nx))
+ru = np.zeros((nt, nx))
+
+
+
+
